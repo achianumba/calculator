f8f560b43e60db8e33f07c1990b0d3d55b44538f
achianumba <chianumbaarinze@gmail.com>
2020-04-24 22:39:05 +0100

    Update: App passes all 16 FCC tests.
    Additonal features of this commit include:
    - An `entry` state that performs a sanity check for '.' entries and
      displays current entries without operators unless the operator
      is a leading negative sign.
    
    - An `equalsPressed` state for tracking '=' button presses and handling
      inputs into `entry` immediately after the '=' sign is pressed.

--- src/index.js
+++ src/index.js
@@ -1,97 +1,170 @@
+/*
+==============
+Imports
+==============
+ */
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Screen from "./components/Screen/Screen";
 import Keypad from "./components/Keypad/Keypad";
-
-export default function Calculator() {
+/*
+===============
+Calculator App
+===============
+ */
+function Calculator() {
+  //App state
   const [display, setDisplay] = useState(0);
   const [entry, setEntry] = useState(0);
+  const [equalsPressed, setEqualsPressed] = useState(false); //track '=' button presses
 
+  //Regex for validating entries
   const isNumber = /\d/;
   const isOperator = /[/x+-]/;
 
+  //Contrary to what you might think,
+  //this function is doesn't determine how
+  //the evaluate() function handles the state of [display] after the '=' sign is pressed.
+  //It only tracks the '=' sign and handles entries into the [entry] state after '=' is pressed
+  const checkPreviousOp = () => {
+    if (equalsPressed && (display === entry)) {
+      setEntry(0);
+      if (equalsPressed) setEqualsPressed(false);
+    }
+  }
+
+  //Numeric keys handler
   const enterNumber = (number) => {
-    return number === "0" && display === 0
-      ? null
-      : number !== 0 && display === 0
-      ? setDisplay(number)
-      : setDisplay(display + number);
+    checkPreviousOp();
+
+    if (number === "0" && display === 0) return;
+    if (String(entry).length === 15) return limitExceeded();
+    
+    if (number !== 0 && display === 0) {
+      setDisplay(number)
+      setEntry(number)
+    } else {
+      setDisplay(display + number);
+      entry === 0 ? setEntry(number) : setEntry(entry + number);
+    }
   };
 
+  //Decimal key handler
   const enterDecimal = (key) => {
-    return String(display).includes(key)
-      ? null
-      : setDisplay(String(display) + key);
+    checkPreviousOp();
+
+    setEqualsPressed(false);
+    if (String(entry).includes(key)) {
+      return;
+    } else {
+      setDisplay(String(display) + key);
+      setEntry(String(entry) + key);
+    }
   };
 
+  //Operator keys handler
   const enterOperator = (operator) => {
-    let temp = String(display), //In case typeof display === 'string'
+    checkPreviousOp();
+
+    let temp = String(display), 
       lastIndex = temp.length - 1,
       lastChar = temp[lastIndex],
       penultimateIndex = lastIndex - 1,
       penultimate = temp[penultimateIndex];
 
-    if (
-      lastChar === operator ||
-      (isOperator.test(penultimate) && isOperator.test(lastChar))
-    )
-      return;
+    if (lastChar === operator) return;
+    if (isOperator.test(penultimate) && isOperator.test(lastChar)) return setDisplay(display.slice(0, penultimateIndex) + operator);
 
     if (operator === "-") {
       if (display === 0) {
-        return setDisplay(operator);
+        setDisplay(operator);
+        return setEntry(operator);
       }
-      return setDisplay(display + operator);
+      
+      setDisplay(display + operator);
+
+      if (entry !== 0 || entry !== '-') setEntry(0)
+      return; 
     }
 
     if (display !== 0 && display !== "-") {
+      setEntry(0);
       return isOperator.test(lastChar)
         ? setDisplay(display.slice(0, lastIndex) + operator)
         : setDisplay(display + operator);
     }
   };
 
+  //Numeric keys handler
   const evaluate = () => {
+    setEqualsPressed(true);
     if (display === 0 || display === "-") return;
-    //3 + 5 * 6 - 2 / 4 should return 32.5
-    let expression = display.replace("x", "*");
-    //just in case fcc puts double decimals directly into the elem
-    let doubleDecimal = /\.{2,}/g;
-    expression.replace(doubleDecimal, ".");
+    let expression = display.replace("x", "*"),
+    result = String(eval(expression));
 
-    return setDisplay(String(eval(expression)));
+    setDisplay(result);
+
+    setEntry(result)
   };
 
-  const clear = () => setDisplay(0);
+  //Clear entries
+  const clear = () => {
+    checkPreviousOp();
+    setDisplay(0);
+    setEntry(0);
+  }
 
-  const del = () => {
-    return display === 0
-      ? null
-      : String(display).length === 1
-      ? setDisplay(0)
-      : setDisplay(display.slice(0, [display.length - 1]));
+  //Backspace handle
+  const backspace = () => {
+    checkPreviousOp();
+
+    if (display === 0) return;
+    
+    if (String(display).length === 1) {
+      setDisplay(0);
+      if (String(entry).length === 1) return setEntry(0);
+      return;
+    } else {
+      setDisplay(display.slice(0, [display.length - 1]));
+      return String(entry).length > 1 ? setEntry(entry.slice(0, [entry.length - 1])) : setEntry(0);
+    }
   };
 
   const toggle = () => {
-    return display === "-" || display === "0"
-      ? null
-      : String(display)[0] === "-"
-      ? setDisplay(display.slice(1))
-      : setDisplay("-" + display);
+    checkPreviousOp();
+
+    if (display === "-" || display === 0) return;
+    
+    if (String(display)[0] === "-") {
+      setDisplay(display.slice(1))
+      setEntry(entry.slice(1))
+    } else {
+      setDisplay("-" + display);
+      setEntry("-" + entry);
+    }
   };
+
+  const limitExceeded = () => {
+    let temp = display;
+
+    setEntry('Limit Exceeded!');
+    setTimeout(() => { setEntry(temp) }, 1000);
+  }
   /*
   Click event handler
   */
   const handler = (e) => {
     e.preventDefault();
+    if (String(entry).includes('Limit')) return;
+
     const id = e.target.id;
     const key = e.target.textContent;
     
     if (id === "equals") return evaluate();
     if (id === "clear") return clear();
     if (id === "cancel") return clear();
-    if (id === "del") return del();
+    if (id === "del") return backspace();
     if (id === "toggle") return toggle();
     if (isOperator.test(key)) return enterOperator(key);
     if (id === "decimal") return enterDecimal(key);
@@ -103,11 +176,13 @@ export default function Calculator() {
     //keyboard events listener/handler
     document.onkeydown = (e) => {
       e.preventDefault();
+      if (String(entry).includes('Limit')) return;
+      
       const key = e.key === "*" ? "x" : e.key;
       
       if (key === "Enter" || key === "=") return evaluate();
       if (key === "Delete") return clear();
-      if (key === "Backspace") return del();
+      if (key === "Backspace") return backspace();
       if (isOperator.test(key)) return enterOperator(key);
       if (key === ".") return enterDecimal(key);
       if (isNumber.test(key)) return enterNumber(key);
